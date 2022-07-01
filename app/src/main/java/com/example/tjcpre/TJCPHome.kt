79639a7a1707e7dda12764c376e3b4adf6bd999a
Luis Bazaldua <Luis29178@gmail.com>
2022-07-01 18:12:12 -0400

    Layout Landscape Ver. Added Action Bar removed tjcphomepage.xml Added --- Completed basic layout and set up relations with working class TJCPHome.kt Added --

---
+++ app/src/main/java/com/example/tjcpre/TJCPHome.kt
@@ -0,0 +1,18 @@
+package com.example.tjcpre
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TJCPHome : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tjcphomepage)
+
+
+
+    }
+
+
+}
