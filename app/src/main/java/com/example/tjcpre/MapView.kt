d536692d4ab61c9b4aa82583d0d686d28d7400a7
Luis Bazaldua <Luis29178@gmail.com>
2022-07-01 17:18:11 -0400

    Layout built

--- app/src/main/java/com/example/tjcpre/MapView.kt
+++ app/src/main/java/com/example/tjcpre/MapView.kt
@@ -13,7 +13,7 @@ class MapView : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_map_pathing)
+        setContentView(R.layout.activity_map_pathing)
 
 
 
