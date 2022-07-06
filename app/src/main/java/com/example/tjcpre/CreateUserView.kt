7265c51fcf695752fe201a7f8faffe2dd523a7dd
Luis Bazaldua <Luis29178@gmail.com>
2022-07-06 04:12:05 -0400

    build.gradle. Project & Module --- Implemented Firebase DDatabase Dependencies and Plugins
    LogInView.kt
    --- Initialized text input
    --- added Function for login in  existing
    --- CreateUser Button OnClick added
          * Switches to CreateUserView.kt
    
    AndroidManifest.xml
    --- Declared CreateUserView.kt Activity
    
    build.gradle
    --- added implementation
    
    create_user_page.xml Added
    
    CreateUserView.kt Added
    --- Responsible for gathering data from the user to create a a database for them
    
    google-services.json Added
    --- dependencies for FireBase
    
    log_in_page.xml
    --- Corrected context for layout

---
+++ app/src/main/java/com/example/tjcpre/CreateUserView.kt
@@ -0,0 +1,74 @@
+package com.example.tjcpre
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class CreateUserView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.create_user_page)
+
+        val UsernameInput  = findViewById<EditText>(R.id.EditCreateUserString)
+        val PasswordInput  = findViewById<EditText>(R.id.EditCreatePassworedString)
+        val ConfirmPasswordInput  = findViewById<EditText>(R.id.EditConfirmPassworedString)
+
+        val auth = FirebaseAuth.getInstance()
+
+        val CreateNewUser = findViewById<Button>(R.id.createnewAccountButton)
+
+
+        CreateNewUser.setOnClickListener {
+            var _UsernameInput : String = UsernameInput.text.toString()
+            var _PasswordInput : String = PasswordInput.text.toString()
+            var _ConfirmPasswordInput : String = ConfirmPasswordInput.text.toString()
+
+            if (_UsernameInput.isBlank() || _PasswordInput.isBlank() || _ConfirmPasswordInput.isBlank()) {
+                Toast.makeText(this, "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
+
+            }
+
+            if (_PasswordInput != _ConfirmPasswordInput) {
+                Toast.makeText(
+                    this,
+                    "Password and Confirm Password do not match",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+            // If all credential are correct
+            // We call createUserWithEmailAndPassword
+            // using auth object and pass the
+            // email and pass in it.
+            auth.createUserWithEmailAndPassword(_UsernameInput, _PasswordInput)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Successfully Singed Up", Toast.LENGTH_SHORT).show()
+                        val user : FirebaseUser = task.result!!.user!!
+                        val intent =
+                            Intent(this@CreateUserView, TJCPHome::class.java)
+                        // removes instances running in the stack when activity is switched
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        intent.putExtra("user_id", user.uid)
+                        intent.putExtra("User_email",_UsernameInput)
+                        startActivity(intent)
+                        finish()
+
+                    } else {
+                        Toast.makeText(this, "Singed Up Failed!", Toast.LENGTH_SHORT).show()
+                    }
+
+
+                }
+        }
+
+
+
+    }
+}
--- app/src/main/java/com/example/tjcpre/LogInView.kt
+++ app/src/main/java/com/example/tjcpre/LogInView.kt
@@ -1,13 +1,13 @@
 package com.example.tjcpre
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-
-
 
 
 class LogInView: AppCompatActivity(){
@@ -23,26 +23,74 @@ class LogInView: AppCompatActivity(){
         setContentView(R.layout.log_in_page)
 
         val LogInButton = findViewById(R.id.Login_button) as Button
-        val CreateAccountButton = findViewById<Button>(R.id.CreateAccountButton)
+        val CreateAccountButton = findViewById(R.id.CreateAccountButton) as Button
 
         val UsernameInput = findViewById<EditText>(R.id.EditUserString)
         val PasswordInput = findViewById<EditText>(R.id.EditPassworedString)
+
+        CreateAccountButton.setOnClickListener{
+
+            val intent = Intent(this,CreateUserView::class.java)
+            startActivity(intent)
+        }
 
         LogInButton.setOnClickListener {
 
             var sendUser : String = UsernameInput.text.toString()
             var sendPassword : String = PasswordInput.text.toString()
 
-            if (sendUser.isNotEmpty()) {
 
-                ReadData(sendUser)
-            }
-            //TODO: Implament fragment to Notify user of Empty parameters
-            if (sendPassword.isNotEmpty())
-            {
+            when{
+                (sendPassword.isEmpty() && sendUser.isEmpty()) -> {
+                    Toast.makeText(
+                        this@LogInView,
+                        "Please enter Username or Email and Password",
+                        Toast.LENGTH_SHORT).show()
+                }
+                sendUser.isEmpty() -> {
+                   Toast.makeText(
+                       this@LogInView,
+                       "Please enter Username or Email",
+                       Toast.LENGTH_SHORT).show()
+
+                }
+                sendPassword.isEmpty() -> {
+                    Toast.makeText(
+                        this@LogInView,
+                        "Please enter Password",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+                else -> {
+                    val InputEmail : String  = sendUser.trim{ it <= ' ' }
+                    val InputPassword : String = sendPassword.trim{ it <= ' '}
+
+                    mAuth.signInWithEmailAndPassword(InputEmail, InputPassword).addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            //TODO: Implement return to home screen with login credentials
+                            Toast.makeText(this, "Successfully LoggedIn", Toast.LENGTH_SHORT).show()
+                            val user : FirebaseUser = task.result!!.user!!
+                            val intent =
+                                Intent(this@LogInView, TJCPHome::class.java)
+                            // removes instances running in the stack when activity is switched
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            intent.putExtra("user_id", user.uid)
+                            intent.putExtra("User_email",InputEmail)
+                            startActivity(intent)
+                            finish()
+
+                        } else {
+                            Toast.makeText(this, "Log In failed ", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }
+
+
 
             }
-            //TODO: Implament fragment to Notify user of Empty parameters
+
+
 
 
 
@@ -50,10 +98,7 @@ class LogInView: AppCompatActivity(){
 
 
         }
-        CreateAccountButton.setOnClickListener {
-            //TODO: Implement Action For Create Login Button
 
-        }
 
 
 
