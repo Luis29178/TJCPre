a9979c8ad40ca2ad57dc8cf6a5d4d97fdecae809
Luis Bazaldua <Luis29178@gmail.com>
2022-07-05 22:23:05 -0400

    build.gradle. Project & Module --- Implemented Firebase DDatabase Dependencies and Plugins
    LogInView.kt
    --- Initialized text input
    --- added Function for communicating with database

--- app/src/main/java/com/example/tjcpre/LogInView.kt
+++ app/src/main/java/com/example/tjcpre/LogInView.kt
@@ -2,9 +2,20 @@ package com.example.tjcpre
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
+
 
 class LogInView: AppCompatActivity(){
+    //lateinit var dataBase : DatabaseRefrence
+    private var mAuth : FirebaseAuth = FirebaseAuth.getInstance();
+
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,14 +25,42 @@ class LogInView: AppCompatActivity(){
         val LogInButton = findViewById(R.id.Login_button) as Button
         val CreateAccountButton = findViewById<Button>(R.id.CreateAccountButton)
 
+        val UsernameInput = findViewById<EditText>(R.id.EditUserString)
+        val PasswordInput = findViewById<EditText>(R.id.EditPassworedString)
+
         LogInButton.setOnClickListener {
-            //TODO: Implement Action For Login Button
+
+            var sendUser : String = UsernameInput.text.toString()
+            var sendPassword : String = PasswordInput.text.toString()
+
+            if (sendUser.isNotEmpty()) {
+
+                ReadData(sendUser)
+            }
+            //TODO: Implament fragment to Notify user of Empty parameters
+            if (sendPassword.isNotEmpty())
+            {
+
+            }
+            //TODO: Implament fragment to Notify user of Empty parameters
+
+
+
+
+
 
         }
         CreateAccountButton.setOnClickListener {
             //TODO: Implement Action For Create Login Button
 
         }
+
+
+
+    }
+
+    private fun ReadData(input: String) {
+
 
 
 
