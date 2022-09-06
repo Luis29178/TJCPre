05a2dfc1f597ef8e086345a1bc133d6bbc86bf2c
Luis Bazaldua <Luis29178@gmail.com>
2022-09-06 03:43:05 -0400

    Net release builds v1

---
+++ NetApp/Release_NetaApp_v1/tjcp_customs/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Canvas from './components/canvas';
+
+function App() {
+  return (
+    <div className="App">
+        
+        <Canvas
+        width={4097}
+        height ={2142}
+        className = {"Canvas"}
+        />
+        
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ NetApp/Release_NetaApp_v1/tjcp_customs/src/components/hooks.js
@@ -0,0 +1,113 @@
+import {useEffect, useRef} from 'react'
+
+export function useOnDraw(onDraw, clear){
+
+   const canRef = useRef(null);
+
+   const isDrawingRef = useRef(false);
+
+   
+
+
+   const mouseMoveListenerRef = useRef(null);
+   const mouseUpListenerRef = useRef(null);
+   const mouseDownListenerRef = useRef(null);
+
+   const prevPointRef = useRef(null);
+
+
+   function setCanvasRef(ref){
+
+    if(!ref) return;
+    if(canRef.current){
+
+    canRef.current.removeEventListener("mousedown", mouseDownListenerRef.current);
+
+    }
+
+    canRef.current = ref
+    initMouseMoveListener();
+    initMouseDownListener();
+    initMouseuUpListener();
+
+   }
+
+   
+
+   function initMouseMoveListener(){
+    const mouseMoveListener = (e)  =>{
+         
+        if(isDrawingRef.current){
+
+            const point = Compointcanvas(e.clientX, e.clientY);
+            const ctx = canRef.current.getContext('2d');
+            if(onDraw) onDraw(ctx,point,prevPointRef.current);
+            prevPointRef.current = point;
+            
+            console.log(point)
+    
+        }
+        
+        
+
+    }
+    mouseMoveListenerRef.current = mouseMoveListener;
+    window.addEventListener("mousemove", mouseMoveListener);
+   }
+
+  
+   function initMouseDownListener(){
+    if (!canRef.current) return;
+    const mdListener = () => {
+
+        isDrawingRef.current = true;
+
+    }
+    mouseDownListenerRef.current = mdListener;
+    canRef.current.addEventListener("mousedown", mdListener);
+
+   }
+
+
+   function initMouseuUpListener(){
+    if (!canRef.current) return;
+    
+    const muListener = () => {
+
+        isDrawingRef.current = false;
+        prevPointRef.current = null;
+        console.log("in befor reset");
+        const ctx = canRef.current.getContext('2d');
+        if(clear) clear(ctx);
+        
+        console.log("in after reset");
+        
+    }
+    mouseUpListenerRef.current = muListener;
+    window.addEventListener("mouseup", muListener);
+    
+
+   }
+
+   
+
+
+   function Compointcanvas(clientX,clientY){
+    
+        if(canRef.current){
+            const boundingRec = canRef.current.getBoundingClientRect();
+            return{
+                x :  clientX - boundingRec.left  ,
+                y : clientY - boundingRec.top
+            }
+        }else{
+            return null;
+        }
+   }
+
+
+
+   return setCanvasRef;
+
+
+};
